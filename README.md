e60adbb3a0726eee1fae9922257e0cf92fc95a04
jetscholar <mrz@jetscholar.com>
2021-01-26 16:27:35 +1100

    1st Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ![jetScholar Logo](./public/assets/QLogo-trans.png)
+
+## Work Log
+
+This is a work log to keep track of what is being done.
+
+## Useful Links
+
+- [Run Server](http://localhost:6801)
+- [Fully Responsive HTML5 & CSS 3 Website Landing Page Project](https://www.youtube.com/watch?v=6cFynufTAac)
+- [Web Dev Tutorial](https://www.youtube.com/watch?v=-RCnNyD0L-s&t=159s)
+- [GitHub - Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
+- [login/reg vid](https://www.youtube.com/watch?v=L5WWrGMsnpw)
+
+## Task List
+
+- [x] Landing Page Styled
+- [ ] Authentication setup
+- [ ] Registration and Login pages styled
+- [ ] Mongo setup and working
+- [ ] User Profile
+
+## Tables
+
+Date | Work Done | Description
+---------- | -------------
+26.01.21 | Built up node.js server |
+27.01.21 | Completed tutorial, app works |
+2x.01.21 | Authentication works |
---
+++ server.js
@@ -0,0 +1,49 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require("express")
+// const morgan = require("morgan")
+const app = express()
+const bcrypt = require('bcrypt')
+const passport =require('passport')
+
+// const initializePassport = require('./routes/passport-config')
+// initializePassport(
+//     passport,
+//     email => users.find(user => user.email === email)
+// )
+
+const bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts')
+const port = 8888
+// app.use(morgan('tiny'))
+
+const indexRouter = require('./routes/index')
+const authenticateRouter = require('./routes/authenticate')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.SECURE_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to the Qudos DB'))
+
+
+// routes
+app.use('/', indexRouter)
+app.use('/authenticate', authenticateRouter)
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server up on http://localhost:${port}`)
+})
